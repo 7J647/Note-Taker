cd5606bf0a2c2dbd4f1bde1c3d5132dc561c76d1
7J647 <007J647@gmail.com>
2020-10-01 16:05:15 -0400

    added first .get routes and a 3rd middleware

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//for all front end files, lets the app know they will all be in the public folder
+app.use(express.static('public'));
 
 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+  });
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+  });
+
+
+
+  
 
 
 app.listen(PORT, function() {
