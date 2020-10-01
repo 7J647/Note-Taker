8d05abb6454f31f4e61c56482d685bf110e81b37
7J647 <007J647@gmail.com>
2020-10-01 15:59:39 -0400

    step 4 listen on the port completed

--- server.js
+++ server.js
@@ -4,3 +4,8 @@ var path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
