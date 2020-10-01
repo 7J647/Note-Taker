b6b89b8744e1d5b2802f502d1f3ddf2426725764
7J647 <007J647@gmail.com>
2020-10-01 16:57:38 -0400

    updated get api/notes route because the data is not coming in as a string, dd not need fs.readFile since we required the db.json

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var fs = require("fs");
 
 const app = express();
 
+const notes =  require("./db/db.json");
+
 const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,11 +19,37 @@ app.use(express.static('public'));
 
 
   app.get("/api/notes", function(req, res) {
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
-    res.json(data);   
-    })
-    
+      //not using this because now it's not coming in as a string 
+    // fs.readFile("./db/db.json", "utf-8", (err, data) => {
+    res.json(notes);   
+    // })  
   });
+
+  app.post("/api/notes", function(req, res) {
+    const newNote = req.body;
+
+    newNote.id ="";
+
+    //npm package for random number or take notes array select last note object
+    //grab that note's id and increment it by 1
+  
+    console.log(newNote);
+  
+    notes.push(newNote);
+
+  
+    //not using err because we are using writeFileSync.   Taking array out of the file
+    //adding a new thing into the array, putting the array back into the file.
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+  
+    res.json(newNote);
+  });
+
+  app.delete("/api/notes/:note"
+
+
+  
+
   
   app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
